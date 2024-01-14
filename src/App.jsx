738d775e5b747c3c98b3fe9c77ece6a6e9ed1c48
Alexander Bogdashkin <a_bogdashkin@mail.ru>
2024-01-14 17:05:20 +0300

    add homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
  import './App.css'
+ import ShopItemFunc from './components/ShopItemFunc';
 
 const item = {
   brand: 'Tiger of Sweden',
@@ -9,25 +10,25 @@ const item = {
   currency: '£'
 }
 
-const ShopItemFunc = () => {
+// const ShopItemFunc = ({item}) => {
   
-  return (
-    <div className="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div className="description">
-        {item.descriptionFull}
-      </div>
-      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-      <div className="divider"></div>
-      <div className="purchase-info">
-        <div className="price">{item.currency}{item.price.toFixed(2)}</div>
-        <button>Добавить в корзину</button>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="main-content">
+//       <h2>{item.brand}</h2>
+//       <h1>{item.title}</h1>
+//       <h3>{item.description}</h3>
+//       <div className="description">
+//         {item.descriptionFull}
+//       </div>
+//       <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+//       <div className="divider"></div>
+//       <div className="purchase-info">
+//         <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+//         <button>Добавить в корзину</button>
+//       </div>
+//     </div>
+//   );
+// };
 
 function App() {
   return (
@@ -38,10 +39,10 @@ function App() {
         <div className='highlight-overlay'></div>
       </div>
       <div className="window">
-        <ShopItemFunc />
+        <ShopItemFunc item={item} />
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
